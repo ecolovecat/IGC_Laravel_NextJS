fa770958414b0d2368f4ca822533ef65b75bcb9b
ecolovecat <tranphuongduy0403@gmail.com>
2023-09-05 20:44:09 +0700

    Configured JWT authentication

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('jwt')->get('/check-jwt', function () {
+    return response()->json(['message' => 'JWT is working']);
+});
+
+
+// Route::middleware('auth:api')->group(function () {
+//     Route::post('/login', [AuthController::class, 'login']);
+//     Route::post('/logout', [AuthController::class, 'logout']);
+//     Route::post('/refresh', [AuthController::class, 'refresh']);
+//     Route::post('/me', [AuthController::class, 'me']);
+// });
