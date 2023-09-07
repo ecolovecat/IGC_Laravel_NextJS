981540ab4a39eaab857e37ba8a71db786b6ee14a
ecolovecat <tranphuongduy0403@gmail.com>
2023-09-07 13:00:00 +0700

    Add migrate

---
+++ backend/database/migrations/2023_09_07_045328_create_follows_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFollowsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('follows', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_following_id');
+            $table->unsignedBigInteger('user_followed_id');
+
+            $table->foreign('user_following_id')->references('id')->on('users');
+            $table->foreign('user_followed_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('follows');
+    }
+}
